2064658fb2ed518eba107b845b01466560f4d883
datradito <datradito@gmail.com>
2022-11-17 10:11:31 -0300

    fix: minor linter

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -4,7 +4,8 @@ export const IS_PRODUCTION = process.env.NEXT_PUBLIC_IS_PRODUCTION
 
 export const GATEWAY_URL_PRODUCTION =
   process.env.NEXT_PUBLIC_GATEWAY_URL_PRODUCTION || 'https://gateway.multisig.moonbeam.network'
-export const GATEWAY_URL_STAGING = process.env.NEXT_PUBLIC_GATEWAY_URL_STAGING || 'https://gateway.staging.multisig.moonbeam.network'
+export const GATEWAY_URL_STAGING =
+  process.env.NEXT_PUBLIC_GATEWAY_URL_STAGING || 'https://gateway.staging.multisig.moonbeam.network'
 
 // Magic numbers
 export const POLLING_INTERVAL = 15_000
