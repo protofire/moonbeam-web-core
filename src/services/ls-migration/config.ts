10d88bef9a95e12e05d8fc7d9ba46318bc142ea5
datradito <datradito@gmail.com>
2022-11-23 08:57:03 -0300

    feat: test migration function with migrationFinished default

--- src/services/ls-migration/config.ts
+++ src/services/ls-migration/config.ts
@@ -1,3 +1,3 @@
 export const IFRAME_HOST = 'https://new.staging.multisig.moonbeam.network'
 export const IFRAME_PATH = '/migrate.html'
-export const MIGRATION_KEY = 'false'
+export const MIGRATION_KEY = 'migrationFinished'
