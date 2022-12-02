c2fe5e5b6d89dd27ce16080deb198c644b9c9ca5
BLAKPC\blak <zhiltsov.nick@gmail.com>
2022-12-02 14:19:33 +0300

    fix: migrate.html path

--- src/services/ls-migration/config.ts
+++ src/services/ls-migration/config.ts
@@ -1,3 +1,3 @@
 export const IFRAME_HOST = 'https://test.staging.multisig.moonbeam.network/'
-export const IFRAME_PATH = '/migrate.html'
+export const IFRAME_PATH = 'migrate.html'
 export const MIGRATION_KEY = 'migrationFinished'
